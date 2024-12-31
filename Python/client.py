19c13df14fd663d5dd90d6fbd09e14586774e689
ZZLAPAS <1473986563@qq.com>
2024-12-31 17:23:17 +0800

    可以简单画图

--- Python/client.py
+++ Python/client.py
@@ -6,16 +6,9 @@ import grpc
 from protos import helloworld_pb2
 from protos import helloworld_pb2_grpc
 
-def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-    print("Will try to greet world ...")
-    with grpc.insecure_channel("localhost:50051") as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name="you"))
-    print("Greeter client received: " + response.message)
-    
+import matplotlib.pyplot as plt
+import numpy as np
+import re
     
 class GreetClient:
     def __init__(self,channel_address='localhost:50051'):
@@ -30,6 +23,49 @@ if __name__ == "__main__":
     logging.basicConfig()
     client = GreetClient()
     
-    response = client.Send_request()
-    print(response)
+    plt.ion()  # 打开交互模式
+    fig, ax = plt.subplots()
+    x_data, y_data = [], []  # 用于存储曲线数据
+    line, = ax.plot(x_data, y_data, label="Response Value")
+
+    # 图表设置
+    ax.set_xlim(0, 50)  # x 轴范围
+    ax.set_ylim(-1, 1)   # y 轴范围（根据 response 数据调整）
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Response")
+    ax.set_title("Real-time Response Plot")
+    ax.legend()
+    
+    i = 1;
+    while(1):
+        response = client.Send_request()
+        print(response)
+        
+        numbers = re.findall(r'-?\d+\.\d+', str(response))
+        
+        x_data.append(i)
+        i = i+1
+        y_data.append(numbers)
+        
+        # 更新数据
+        line.set_xdata(x_data)
+        line.set_ydata(y_data)
+        
+        # 动态调整 x 轴范围
+        if i > 50:  # 当数据超过 50 时，向右滚动
+            ax.set_xlim(i - 50, i)
+        
+        current_ylim = ax.get_ylim()    
+        if float(numbers[0]) > current_ylim[1]:  # 当数据超过 50 时，向右滚动
+            ax.set_ylim(current_ylim[0],current_ylim[1]+1)
+        if float(numbers[0]) < current_ylim[0]:  # 当数据超过 50 时，向右滚动
+            ax.set_ylim(current_ylim[0]-1,current_ylim[1])
+        
+        
+        # 刷新图表
+        plt.pause(0.01)  # 暂停 0.1 秒用于绘图
+        fig.canvas.draw()
+
+    plt.ioff()  # 关闭交互模式
+    plt.show()
     #run()
--- Python/server.py
+++ Python/server.py
@@ -5,12 +5,18 @@ import grpc
 from protos import helloworld_pb2
 from protos import helloworld_pb2_grpc
 
+import threading
+import time
+
+i = 0
+
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
-        return helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
+        return helloworld_pb2.HelloReply(message="Hello%d, %s!" %(i, request.name))
 
 
 def serve():
+    #port = "50051"
     port = "50051"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
@@ -22,4 +28,18 @@ def serve():
 
 if __name__ == "__main__":
     logging.basicConfig()
-    serve()
+    thread1 = threading.Thread(target=serve)
+    thread1.start()
+    
+    
+    while(1):
+        i = i+1
+        time.sleep(1)
+        if i == 5:
+            i = 0
+        print(i)
+        
+    thread1.join()
+            
+            
+        
